59d7fa4e08f3c2997ebef867a80763d8548ad38d
Sean Rettig <seanrettig@gmail.com>
2023-04-08 19:02:02 -0700

    keep score

---
+++ HUD.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+export var score = 0
+export var isCounting = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$ScoreLabel.text = str(score)
