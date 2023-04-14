14beffc660fda0f494ef48a50d229daf4eaaed0b
Sean Rettig <seanrettig@gmail.com>
2023-04-14 03:26:28 -0700

    fix old pipes not being deleted and fix still using old TopPipe.gd script

--- Pipe.gd
+++ Pipe.gd
@@ -1,18 +1,10 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export var pipe_speed = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	self.linear_velocity = Vector2.LEFT * pipe_speed
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
